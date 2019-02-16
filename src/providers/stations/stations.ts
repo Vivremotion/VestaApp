a9717bcd5fd0205122cf6f9000fa3d3e159b4788
tandy02 <cyril.tandy@etu.unilim.fr>
2019-02-16 11:22:49 +0100

    Refactor Stations provider

--- src/providers/stations/stations.ts
+++ src/providers/stations/stations.ts
@@ -4,40 +4,65 @@ import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 import { BehaviorSubject } from "rxjs";
 
 import { Station } from '../../models/station';
+import {Network} from "../../models/network";
 
 @Injectable()
 export class Stations {
   stations: Station[];
   stationsSubject: BehaviorSubject<Station[]> = new BehaviorSubject([]);
+  connectedStations = [];
   connectedStationsSubject: BehaviorSubject<Station[]> = new BehaviorSubject([]);
+  availableStationsSubject: BehaviorSubject<Station[]> = new BehaviorSubject([]);
 
   constructor(public storage: Storage, public bluetooth: BluetoothSerial) {
     this.getStored().then((storedStations => {
-      storedStations.forEach((station, index, array) => array[index].connected = false);
+      storedStations.forEach((station, index, array) => array[index].bluetoothConnected = false);
       this.stations = storedStations;
       this.stationsSubject.next(storedStations);
       this.stationsSubject.subscribe({
         next: function (stations) {
-          const connectedStations = stations.filter(station => station.connected);
-          this.connectedStationsSubject.next(connectedStations);
+          this.connectedStations = stations.filter(station => station.bluetoothConnected);
+          this.connectedStationsSubject.next(this.connectedStations);
         }.bind(this)
       });
     }));
+
+    this.scanBluetooth();
+    setInterval(this.scanBluetooth.bind(this), 60000);
   }
 
-  connect(station: Station) {
+  scanBluetooth() {
+    this.bluetooth.enable()
+      .then(() => {
+        this.bluetooth.discoverUnpaired()
+          .then(devices => {
+            console.log(devices);
+            console.log(this.stations);
+            const availableDevices = devices.filter((device) => {
+              return !this.connectedStations.find((station) => device.id === station.id);
+            });
+            this.availableStationsSubject.next(availableDevices);
+
+          })
+          .catch(error => console.error(error));
+        }
+      )
+      .catch(error => console.error(error));
+  }
+
+  connectBluetooth(station: Station) {
     const deviceConnection = this.bluetooth.connect(station.address);
     return new Promise((resolve, reject) => {
       deviceConnection.subscribe({
         next: function() {
-          station.connected = true;
+          station.bluetoothConnected = true;
           this.upsert(station);
           this.bluetooth.write(JSON.stringify({route: 'Test/do'}))
             .catch(e => console.error(e));
           resolve(true);
         }.bind(this),
         error: function(error) {
-          station.connected = false;
+          station.bluetoothConnected = false;
           this.upsert(station);
           console.error(error);
           reject(error);
@@ -46,17 +71,34 @@ export class Stations {
     });
   }
 
-  disconnect(station) {
+  disconnectBluetooth(station) {
     this.bluetooth.disconnect()
       .then(_ => {
-        station.connected = false;
+        station.bluetoothConnected = false;
         this.upsert(station);
       });
+  }
+
+  requestAvailableNetworks(station) {
+    this.bluetooth.write(JSON.stringify({
+      route: 'Wifi/getAll'
+    }));
+  }
+
+  setAvailableNetworks(stationId: string, networks: Array<Network>) {
+    const station = this.find(stationId);
+    station.networks = networks;
+    this.upsert(station);
   }
 
   watchConnected(observer) {
     this.connectedStationsSubject.subscribe(observer);
     return { unsubscribe: this.connectedStationsSubject.unsubscribe };
+  }
+
+  watchAvailable(observer) {
+    this.availableStationsSubject.subscribe(observer);
+    return { unsubscribe: this.availableStationsSubject.unsubscribe };
   }
 
   upsert(station: Station) {
@@ -76,6 +118,10 @@ export class Stations {
       .then(stations => {
         return JSON.parse(stations) || [];
       });
+  }
+
+  find(id: string) {
+    return this.stations.find(station => station.id === id);
   }
 
   delete(station: Station) {
